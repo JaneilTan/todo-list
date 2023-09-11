f886f51dc161d362f11dd71a3d54be2519275676
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-09-11 21:31:31 +1200

    Update for code quality

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,10 @@
-import { useFruits } from '../hooks/useFruits.ts'
 import  AddTasks  from './AddTask.tsx'
-import { Todo } from './todo.tsx'
+import { Todo } from './Todo.tsx'
 
 
 function App() {
   return (
     <>
-     <h1>To Do:</h1>
      <AddTasks />
      <Todo />
     </>
